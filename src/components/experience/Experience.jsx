58a818c49bdcb24d5ffd3219dfa87defd189feed
PeekMe01 <ralphdaher6@gmail.com>
2023-06-11 13:19:06 +0300

    Update Experience.jsx
    experience jsx file

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,11 +1,83 @@
 import React from 'react'
 import './experience.css'
+import {MdVerified} from 'react-icons/md'
 
 const Experience = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Skills</h2>
+      <div className="container experience__container">
+        <div>
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <MdVerified className='experience__details-icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <MdVerified className='experience__details-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <MdVerified className='experience__details-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Intermidiate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <MdVerified className='experience__details-icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Intermidiate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <MdVerified className='experience__details-icon'/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Intermidiate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONTEND */}
+
+        <div>
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <MdVerified className='experience__details-icon'/>
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <MdVerified className='experience__details-icon'/>
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Basics</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <MdVerified className='experience__details-icon'/>
+              <div>
+                <h4>Entity Framework</h4>
+                <small className='text-light'>Intermidiate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
