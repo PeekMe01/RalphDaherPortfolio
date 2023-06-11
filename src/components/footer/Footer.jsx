ac3b8917ba97b1e8c508a5979a8b3c5f7a0f9fa1
PeekMe01 <ralphdaher6@gmail.com>
2023-06-11 19:52:27 +0300

    Update Footer.jsx
    added footer jsx file

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer>
+      <a href="#" className='footer__logo'>Ralph</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/ralph.daher.75"><FaFacebookF/></a>
+        <a href="https://www.instagram.com/daher.ralph/"><FaInstagram/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; PeekMe. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
