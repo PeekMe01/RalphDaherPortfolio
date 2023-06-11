8eae7fd21142c6016674a391eab5f0ca612b5162
PeekMe01 <ralphdaher6@gmail.com>
2023-06-11 13:20:10 +0300

    Update Portfolio.jsx
    portfolio jsx file

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,9 +3,16 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <div>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-    </div>
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          
+        </article>
+      </div>
+    </section>
   )
 }
 
