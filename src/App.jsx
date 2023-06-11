2f2d3c6d8a178205e4fb3ad8d7780ee9321ff02b
PeekMe01 <ralphdaher6@gmail.com>
2023-06-11 13:18:14 +0300

    Update App.jsx
    fixed Header import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/header/Header'
+import Header from './components/header/Header.jsx'
 import Nav from './components/nav/Nav.jsx'
 import About from './components/about/About.jsx'
 import Experience from './components/experience/Experience.jsx'
