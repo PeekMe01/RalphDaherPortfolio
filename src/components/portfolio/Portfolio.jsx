f2eb7243de51b0cf417fb0f63e04da1c26bc3e32
PeekMe01 <ralphdaher6@gmail.com>
2023-06-11 19:52:54 +0300

    Update Portfolio.jsx
    added portfolio jsx file

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,50 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/project1.png'
+import IMG2 from '../../assets/project2.png'
+import IMG3 from '../../assets/project3.png'
+import IMG4 from '../../assets/project4.png'
+import IMG5 from '../../assets/project5.png'
+import IMG6 from '../../assets/project6.png'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Car Dealership Management',
+    github: 'https://github.com/HyperJAK/CarDealerShip_Project'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Hospital Management System',
+    github: 'https://github.com/PeekMe01/Hospital-Management-System'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'UI-UX W4Kitchen cooking website & app Design',
+    figma: 'https://www.figma.com/file/7Ki5oDpHGewYVtXTnNBfpy/UI%2FUX-Design-Project-Sketches?type=design&node-id=0%3A1&t=igXEUCjOWloGndta-1'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Student add/remove To Database',
+    github: 'https://github.com/PeekMe01/Student-add-remove-to-Database'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'AI hand tracking and finger counter',
+    github: 'https://github.com/PeekMe01/AIFingerCountingProject'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Multilayer Perceptron Back Propagation GUI',
+    github: 'https://github.com/PeekMe01/MultiLayerPerceptron'
+  },
+]
 
 const Portfolio = () => {
   return (
@@ -8,9 +53,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          
-        </article>
+        {
+          data.map(({id,image,title,github,figma}) =>{
+            //console.log(figma===undefined);
+            return(
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github===undefined?figma:github} className='btn' target='_blank'>Github</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
