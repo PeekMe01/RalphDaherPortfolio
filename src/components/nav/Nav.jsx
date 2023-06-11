fbbcf2a61f3df6d0da09874add929f32e28fd8e9
PeekMe01 <ralphdaher6@gmail.com>
2023-06-11 13:20:02 +0300

    Update Nav.jsx
    nav jsx file

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {BiNetworkChart, BiMessageRounded} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === '#'? 'active' : ' '}><AiOutlineHome/></a>
+      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about'? 'active' : ' '}><AiOutlineUser/></a>
+      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience'? 'active' : ' '}><BiBook/></a>
+      <a href="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio'? 'active' : ' '}><BiNetworkChart/></a>
+      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact'? 'active' : ' '}><BiMessageRounded/></a>
+    </nav>
   )
 }
 
