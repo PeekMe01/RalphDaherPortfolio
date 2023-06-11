08ea39b2a4a1ef95892d7e365329ba40bdfed120
PeekMe01 <ralphdaher6@gmail.com>
2023-06-11 13:19:32 +0300

    Update Header.jsx
    Header jsx file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/1683214357815.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,6 +12,13 @@ const Header = () => {
         <h1>Ralph Daher</h1>
         <h5 className="text-light">University Student</h5>
         <CTA />
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
